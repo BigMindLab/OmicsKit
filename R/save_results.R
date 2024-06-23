2362df4e9e911b37440c610be01bbb0e392d7ee9
David Requena <d.requena.a@gmail.com>
2024-06-23 03:18:56 -0400

    Uploading David's past functions

---
+++ R/save_results.R
@@ -0,0 +1,37 @@
+#########################
+# Function save_results #
+#########################
+
+#' This function will save the results of a Differential Expression analysis
+#'
+#' This function takes as input the output of the function "results()" of DEseq2.
+#' And will save 3 tables:
+#' - A table with all genes
+#' - A table including only the over-expressed genes
+#' - A table including only the under-expressed genes
+#'
+#' @param df A dataframe with the results of a Differential Expression analysis.
+#' @param name The name to be used to save the tables, without file extension.
+#' @param l2fc The cut-off of Log2(Fold Change) for the over- and under-expressed tables. Default = 0.
+#' @export
+
+save_results <- function(df, name, l2fc = 0){
+  
+  names(df)[names(df) == "padj"] <- "FDR"
+  
+  # Saving all genes:
+  write.xlsx(df, colNames = T, rowNames = F, append = F,
+             file = paste0(name, "_full.xlsx"), overwrite = T)
+
+  #Saving over-expressed genes:
+  df.sig.fold_over <- subset(df, ((FDR < cutoff_alpha) & !is.na(FDR)) &
+                               log2FoldChange >= l2fc)
+  write.xlsx(df.sig.fold_over, colNames = T, rowNames = F, append = F,
+             file = paste0(name, "_Overexp.xlsx"), overwrite = T)
+
+  #Saving under-expressed genes:
+  df.sig.fold_under <- subset(df, ((FDR < cutoff_alpha) & !is.na(FDR)) &
+                                log2FoldChange <= -l2fc)
+  write.xlsx(df.sig.fold_under, colNames = T, rowNames = F, append = F,
+             file = paste0(name, "_Underexp.xlsx"), overwrite = T)
+}
