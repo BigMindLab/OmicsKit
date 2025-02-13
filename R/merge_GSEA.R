a8630593bba01de5fa447b9c8c23ede17dd587b5
DanielGarbozo <dan.garbozo.1201.urp@gmail.com>
2025-02-13 01:32:28 -0500

    Add documentation for merge_GSEA

--- R/merge_GSEA.R
+++ R/merge_GSEA.R
@@ -1,21 +1,25 @@
 
-# Merge GSEA DATA into a unique table # 
+# Function merge_GSEA #  
 
-#' Merge collection results from multiple output GSEA files from broad institute into a single table.
+#' Merge GSEA results data frames.
+#' 
 #' After run GSEA_all.sh from GSEA.sh, move the .tsv files to a single directory 
 #' 
 #' @param input_directory The directory containing the GSEA collection results in TSV format.
 #' @param output_file The output file to save the merged data. If not provided, the file will be saved in the input directory.
-#' @importFrom dplyr bind_rows mutate select relocate rename filter if_any
-#' @importFrom readr read_tsv write_tsv
-#' @importFrom tidyr separate
 #' @importFrom magrittr %>%
-#' @importFrom base sub basename ifelse file.path dir.exists list.files stop commandArgs length print cat
-#' @importFrom stringr sub grepl str_replace
 #' @export
  
 
 merge_GSEA <- function(input_directory, output_file) {
+  
+  if (!requireNamespace("dplyr", quietly = TRUE)) {
+    stop(
+      "Package \"dplyr\" must be installed to use this function.",
+      call. = FALSE
+    )
+  }
+ 
   # Read command-line arguments
   args <- commandArgs(trailingOnly = TRUE)
   if (length(args) < 1) {
@@ -36,47 +40,47 @@ merge_GSEA <- function(input_directory, output_file) {
   
   # Function to read each file and add a column with the modified file name
   read_file <- function(file) {
-    data <- read_tsv(file)
+    data <- readr::read_tsv(file)
     file_name <- basename(file)
     file_name <- sub("_all.tsv$", "", file_name)  # Change the pattern if necessary
     # Convert all numeric columns explicitly
     numeric_cols <- c("SIZE", "ES", "NES", "NOM p-val", "FDR q-val", "FWER p-val", "RANK AT MAX")
     data <- data %>%
-      mutate(across(all_of(numeric_cols), as.numeric))
+      dplyr::mutate(across(tidyselect::all_of(numeric_cols), as.numeric))
     data$COLLECTION <- file_name
     return(data)
   }
 
   # Read and combine all TSV files into a single data frame
-  gsea_data <- lapply(files, read_file) %>% bind_rows()
+  gsea_data <- lapply(files, read_file) %>% dplyr::bind_rows()
   
   gsea_data <- gsea_data %>%
-    select(-`...12`)
+    dplyr::select(-`...12`)
   
   # Define numeric columns
   numeric_cols <- c("SIZE", "ES", "NES", "NOM p-val", "FDR q-val", "FWER p-val", "RANK AT MAX")
   
   # Find problematic values in numeric columns
   gsea_data %>%
-    filter(if_any(all_of(numeric_cols), ~ !grepl("^-?[0-9.]+$", .))) %>%
+    dplyr::filter(dplyr::if_any(all_of(numeric_cols), ~ !grepl("^-?[0-9.]+$", .))) %>%
     print()
   
   # Data processing: selection, separation, mutation, and renaming of columns
   gsea_data <- gsea_data %>%
-    select(-"GS<br> follow link to MSigDB", -"GS DETAILS") %>%  #, -"...12"
-    separate(col = `LEADING EDGE`, into = c("tags", "list", "signal"), sep = ",", remove = FALSE) %>%
-    mutate(
+    dplyr::select(-"GS<br> follow link to MSigDB", -"GS DETAILS") %>%  #, -"...12"
+    tidyr::separate(col = `LEADING EDGE`, into = c("tags", "list", "signal"), sep = ",", remove = FALSE) %>%
+    dplyr::mutate(
       tags = 0.01 * as.numeric(sub("%", "", sub("tags=", "", tags))),
       list = 0.01 * as.numeric(sub("%", "", sub("list=", "", list))),
       signal = 0.01 * as.numeric(sub("%", "", sub("signal=", "", signal))),
       `FDR q-val` = ifelse(`FDR q-val` == 0, 0.001, `FDR q-val`),
       `Log10FDR` = -log10(`FDR q-val`)
     ) %>%
-    relocate(`Log10FDR`, .after = `FWER p-val`) %>%
-    rename(COMPARISON = Comparison, FDR = `FDR q-val`)
+    dplyr::relocate(`Log10FDR`, .after = `FWER p-val`) %>%
+    dplyr::rename(COMPARISON = Comparison, FDR = `FDR q-val`)
   
   # Save the processed data to a TSV file
-  write_tsv(gsea_data, output_file)
+  readr::write_tsv(gsea_data, output_file)
   
   cat("GSEA data saved to:", output_file, "\n")
 }
